cd081b4b91c4fbb94a1c2ee8ff0a960e2e88fdb8
Debayan <debayanghosh408@gmail.com>
2024-04-26 21:20:49 +0530

    qs 2

---
+++ qs2.cpp
@@ -0,0 +1,24 @@
+//Pallindrome Number QS-02
+//Input: x = 121
+//Output: true
+//Explanation: 121 reads as 121 from left to right and from right to left.
+class Solution {
+public:
+    bool isPalindrome(int x) {
+      //check for negative
+      if(x < 0){
+	      return false;
+      }
+      long long reversed = 0;
+      long long temp = x;
+      while(temp != 0){
+	      int digit = temp % 10;
+	      reversed = reversed * 10 + digit;
+	      temp /= 10;
+      }
+      return (reversed == x);
+		    
+      
+        
+    }
+};
